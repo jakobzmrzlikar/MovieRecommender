e3cfa4f911ea7f367dbf96bbab6b78e1fbfb8731
Jakob <jakob.zmrzlikar@gmail.com>
2021-05-05 14:51:41 +0200

    added View and Rating models

--- movie_recommender/movies/models.py
+++ movie_recommender/movies/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -6,7 +8,21 @@ class Movie(models.Model):
     year = models.PositiveIntegerField()
     date_published = models.DateField()
     genre = models.CharField(max_length=255)
-    duration = models.PositiveBigIntegerField()
+    duration = models.PositiveIntegerField()
     country = models.CharField(max_length=255)
     language = models.CharField(max_length=255)
     director = models.CharField(max_length=255)
+
+
+class View(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    num_rating = models.PositiveIntegerField()
+    text_rating = models.TextField()
