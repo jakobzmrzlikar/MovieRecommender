36d003655e8c1f6c775660a1beaf2ba621b9981c
Jakob <jakob.zmrzlikar@gmail.com>
2021-05-05 14:50:17 +0200

    moved index to app 'movies'

--- movie_recommender/accounts/urls.py
+++ movie_recommender/accounts/urls.py
@@ -4,8 +4,7 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'accounts'
 urlpatterns = [
-    path('', views.index, name="home"),
     path('accounts/sign_up/', views.sign_up, name="sign-up")
 ]
